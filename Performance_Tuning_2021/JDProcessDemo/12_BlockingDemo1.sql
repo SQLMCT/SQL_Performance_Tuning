8d20a0f2f6e105482c03d5704af68a8514397811
SQLMCT <33372273+SQLMCT@users.noreply.github.com>
2021-04-01 13:32:49 -0400

    Updated Demo

---
+++ Performance_Tuning_2021/JDProcessDemo/12_BlockingDemo1.sql
@@ -0,0 +1,20 @@
+--Blocking Demo
+--Jack updates the Person.Person table
+
+BEGIN TRAN
+UPDATE Person.Person
+SET FirstName = 'Jack', LastName = 'Frost'
+WHERE BusinessEntityID = 18
+
+
+
+
+
+
+
+
+
+
+
+
+

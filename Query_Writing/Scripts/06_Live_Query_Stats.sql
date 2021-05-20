383c15ad0992f3d585e13104ccaad60f78675432
SQLMCT <33372273+SQLMCT@users.noreply.github.com>
2021-05-20 14:04:51 -0400

    Move Files

--- Query_Writing/Scripts/06_Live_Query_Stats.sql
+++
@@ -1,9 +0,0 @@
-
-USE AdventureWorks2016;
-GO
- 
-SELECT * 
-FROM Production.TransactionHistory AS TranHist
-INNER JOIN Production.TransactionHistoryArchive AS TranHistArch 
-ON TranHist.Quantity = TranHistArch.Quantity;
-GO

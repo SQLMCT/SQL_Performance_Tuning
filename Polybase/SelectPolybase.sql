626db9025c02ed772ee77d6267b78c3a702831d8
SQLMCT <john@deardurff.com>
2023-01-21 13:02:32 -0500

    Update Slides

--- Polybase/SelectPolybase.sql
+++
@@ -1,5 +0,0 @@
-SELECT  SUM(f.[SalesAmount]) AS [sales_by_brand_amount]
-,       p.[BrandName]
-FROM    [asb].[FactOnlineSales] AS f
-JOIN    [asb].[DimProduct]      AS p ON f.[ProductKey] = p.[ProductKey]
-GROUP BY p.[BrandName]
